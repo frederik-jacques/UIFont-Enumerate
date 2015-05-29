edfae72a1d28f11225dfb8e767d21b35855dc051
Frederik Jacques <frederik@the-nerd.be>
2015-05-29 11:58:29 +0200

    updated podspec

--- Pod/Classes/UIFont-Enumerate.swift
+++ Pod/Classes/UIFont-Enumerate.swift
@@ -1,5 +1,5 @@
 //
-//  UIFont+Enumerate.swift
+//  UIFont-Enumerate.swift
 //
 //  Created by Frederik Jacques on 28/05/15.
 //  Copyright (c) 2015 the-nerd. All rights reserved.
@@ -8,25 +8,25 @@
 import UIKit
 
 extension UIFont {
-    
+
     class func enumerateFonts(){
-        
+
         println("[UIFont] Start enumerating \(UIFont.familyNames().count) font families")
-                
+
         for( i, fontFamily ) in enumerate(UIFont.familyNames()) {
-            
+
             println("\(i+1). Font family = \(fontFamily as! String)");
-            
+
             for( j, fontName ) in enumerate(UIFont.fontNamesForFamilyName( fontFamily as! String )) {
-            
+
                 println("- \(j+1)) Font name = \(fontName as! String)");
-                
+
             }
-            
+
             println("\n");
         }
-        
+
         println("[UIFont] End enumerating fonts")
     }
-    
+
 }
