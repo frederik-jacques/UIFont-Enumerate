8daa4a355b59b554c5cf0e9dd9453d16b696cce9
Frederik Jacques <frederik@the-nerd.be>
2015-05-29 15:42:22 +0200

    added header file

---
+++ Pod/Classes/UIFont_Enumerate.h
@@ -0,0 +1,17 @@
+//
+//  UIFont_Enumerate.h
+//  UIFont_Enumerate
+//
+//  Created by Frederik Jacques on 29-05-2015.
+//  Copyright (c) 2015 the-nerd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIFont_Enumerate.
+FOUNDATION_EXPORT double UIFont_EnumerateVersionNumber;
+
+//! Project version string for UIFont_Enumerate.
+FOUNDATION_EXPORT const unsigned char UIFont_EnumerateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIFont_Enumerate/PublicHeader.h>
